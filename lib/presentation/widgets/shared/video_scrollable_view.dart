09523e5ddf5276b8c2fdd5d2d2ef394ba4fe1092
LeopoldoIII <nathan.rx@gmail.com>
2023-10-18 23:00:03 -0600

    Controlador del video - FutureBuilder

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -21,7 +22,12 @@ class VideoScrollableView extends StatelessWidget {
         return Stack(
           children: [
             //Video player
-
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              ),
+            ),
             // Botones
             Positioned(
               bottom: 40,
